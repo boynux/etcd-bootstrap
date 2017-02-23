91e107453c794629bb15d1dec1d08dd9f4daffa7
Mohammad Arab <mohammad.arab@stylight.com>
2017-02-23 14:18:23 +0100

    Add more tests for ETCD abstraction

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -1,52 +1,16 @@
 package etcd
 
 import (
-	"bytes"
 	"log"
-	"text/template"
 	"time"
 
 	"github.com/coreos/etcd/client"
 	"golang.org/x/net/context"
 )
 
-var (
-	argsTemplate = []string{
-		`--name {{.Name}}`,
-		`--listen-client-urls http://{{.PrivateIP}}:2379,http://127.0.0.1:2379`,
-		`--initial-advertise-peer-urls http://{{.PrivateIP}}:2380`,
-		`--listen-peer-urls http://{{.PrivateIP}}:2380`,
-		`--advertise-client-urls http://{{.PrivateIP}}:{{.ClientPort}}`,
-		`--initial-cluster-token {{printf "%x" .Token}}`,
-		`--initial-cluster {{ call .Join .Peers ","}}`,
-		`--initial-cluster-state {{.ClusterState}}`,
-	}
-
-	envTemplate = []string{
-		`ETCD_NAME={{.Name}}`,
-		`ETCD_ADVERTISE_CLIENT_URLS=http://{{.PrivateIP}}:{{.ClientPort}}`,
-		`ETCD_LISTEN_PEER_URLS=http://{{.PrivateIP}}:2380`,
-		`ETCD_INITIAL_ADVERTISE_PEER_URLS=http://{{.PrivateIP}}:2380`,
-		`ETCD_LISTEN_CLIENT_URLS=http://{{.PrivateIP}}:{{.ClientPort}},http://127.0.0.1:{{.ClientPort}}`,
-		`ETCD_INITIAL_CLUSTER_TOKEN={{printf "%x" .Token}}`,
-		`ETCD_INITIAL_CLUSTER={{ call .Join .Peers ","}}`,
-		`ETCD_INITIAL_CLUSTER_STATE={{.ClusterState}}`,
-	}
-)
-
 type Client client.Client
 type MembersAPI client.MembersAPI
 type Member client.Member
-
-type Parameters struct {
-	Name         string
-	PrivateIP    string
-	ClientPort   int
-	Peers        []string
-	Token        [16]byte
-	ClusterState string
-	Join         func([]string, string) string
-}
 
 type Etcd struct {
 	endPoints []string
@@ -74,31 +38,6 @@ func (e *Etcd) GarbageCollector(c context.Context, members []string) {
 			}
 		}
 	}
-}
-
-func GenerateParameteres(output string, params Parameters) []string {
-	args := make([]string, len(argsTemplate))
-
-	temp := argsTemplate
-	if output == "env" {
-		temp = envTemplate
-	}
-	for x, i := range temp {
-		tmpl, err := template.New("etcd-args").Parse(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var b bytes.Buffer
-		err = tmpl.Execute(&b, params)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		args[x] = b.String()
-	}
-
-	return args
 }
 
 func New(endpoints ...string) (*Etcd, error) {
--- etcd/etcd_test.go
+++ etcd/etcd_test.go
@@ -1,24 +1,41 @@
 package etcd
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
 	"github.com/coreos/etcd/client"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
 )
 
 type fakeTransport struct {
 	client.CancelableTransport
 
-	respchan     chan *http.Response
-	errchan      chan error
-	startCancel  chan struct{}
-	finishCancel chan struct{}
+	response *http.Response
+}
+
+func (t *fakeTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	fmt.Println(*req.URL)
+	if t.response == nil {
+		return nil, errors.New("Can't work!")
+	} else {
+		t.response.Request = req
+		return t.response, nil
+	}
 }
 
 func TestNewClient(t *testing.T) {
+	originalTransport := client.DefaultTransport
 	client.DefaultTransport = &fakeTransport{}
+	defer func() {
+		client.DefaultTransport = originalTransport
+	}()
 
 	_, err := New("test-endpoint")
 
@@ -26,10 +43,109 @@ func TestNewClient(t *testing.T) {
 }
 
 func TestNewMembersAPI(t *testing.T) {
+	originalTransport := client.DefaultTransport
 	client.DefaultTransport = &fakeTransport{}
+	defer func() {
+		client.DefaultTransport = originalTransport
+	}()
 
 	c, _ := New("test-endpoint")
 	m := c.NewMembersAPI()
 
 	assert.NotNil(t, m)
+}
+
+func TestGetLeader(t *testing.T) {
+	member := Member{
+		ID:         "5b4af01f12132171",
+		Name:       "test-1",
+		PeerURLs:   []string{"http://127.0.0.1:2380"},
+		ClientURLs: []string{"http://127.0.0.1:2379"},
+	}
+
+	responseBody, _ := json.Marshal(member)
+	originalTransport := client.DefaultTransport
+	client.DefaultTransport = &fakeTransport{
+		response: &http.Response{
+			Status:     "200 OK",
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewReader(responseBody)),
+		},
+	}
+	defer func() {
+		client.DefaultTransport = originalTransport
+	}()
+
+	c, _ := New("test-endpoint")
+	leader, err := c.GetLeader(context.Background())
+
+	assert.NoError(t, err)
+	assert.Equal(t, "5b4af01f12132171", leader.ID)
+}
+
+func TestListMembers(t *testing.T) {
+	members := struct {
+		Members []*Member `json:"members"`
+	}{
+		Members: []*Member{
+			&Member{
+				ID:         "5b4af01f12132171",
+				Name:       "test-1",
+				PeerURLs:   []string{"http://127.0.0.1:2380"},
+				ClientURLs: []string{"http://127.0.0.1:2379"},
+			},
+			&Member{
+				ID:         "c5abd02102276712",
+				Name:       "test-2",
+				PeerURLs:   []string{"http://127.0.0.2:2380"},
+				ClientURLs: []string{"http://127.0.0.2:2379"},
+			},
+		},
+	}
+
+	responseBody, _ := json.Marshal(members)
+	originalTransport := client.DefaultTransport
+	client.DefaultTransport = &fakeTransport{
+		response: &http.Response{
+			Status:     "200 OK",
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewReader(responseBody)),
+		},
+	}
+	defer func() {
+		client.DefaultTransport = originalTransport
+	}()
+
+	c, _ := New("test-endpoint")
+	m, err := c.ListMembers(context.Background())
+
+	fmt.Println(m)
+	assert.NoError(t, err)
+	assert.EqualValues(t, members.Members, m)
+}
+
+func TestAddMember(t *testing.T) {
+	member := Member{
+		ID:       "5b4af01f12132171",
+		PeerURLs: []string{"http://127.0.0.1:2380"},
+	}
+
+	responseBody, _ := json.Marshal(member)
+	originalTransport := client.DefaultTransport
+	client.DefaultTransport = &fakeTransport{
+		response: &http.Response{
+			Status:     "201 OK",
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(bytes.NewReader(responseBody)),
+		},
+	}
+	defer func() {
+		client.DefaultTransport = originalTransport
+	}()
+
+	c, _ := New("test-endpoint")
+	m, err := c.AddMember(context.Background(), "http://127.0.0.1:2380")
+
+	assert.NoError(t, err)
+	assert.Equal(t, "5b4af01f12132171", m.ID)
 }
